a91ec897c80fe62ce0a514384431350cef3597d5
itec-barraza <63619807+itec-barraza@users.noreply.github.com>
2021-07-16 21:43:02 -0300

    Update mongo.js

--- src/mongo.js
+++ src/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //Connection URL
-const connectString = 'mongodb+srv://admin:admin@cluster0.pqhsd.mongodb.net/agenda?retryWrites=true&w=majority'
+const connectString = 'mongodb+srv://<user>:<password>@cluster0.pqhsd.mongodb.net/<database>?retryWrites=true&w=majority'
 console.log('Conexión a BDD')
 bdd = mongoose.connect(connectString, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>console.log('Conexión exitosa'))
